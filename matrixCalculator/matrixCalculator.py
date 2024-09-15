25b951b0b8362fd71bd72ba33c44315e0dcea856
Vitalii Kovtun <VITALII.KOVTUN@lnu.edu.ua>
2024-09-15 22:41:11 +0300

    Matrix and Vector calculators added

---
+++ matrixCalculator/matrixCalculator.py
@@ -0,0 +1,110 @@
+def add(matrix1, matrix2):
+    matrix1Rows, matrix1Cols = len(matrix1), len(matrix1[0])
+    matrix2Rows, matrix2Cols = len(matrix2), len(matrix2[0])
+
+    if matrix1Rows != matrix2Rows or matrix1Cols != matrix2Cols:
+        raise Exception("Matrix size mismatch")
+
+    resultMatrix = [[0 for _ in range(matrix1Cols)] for _ in range(matrix1Rows)]
+
+    for i in range(matrix1Rows):
+        for j in range(matrix1Cols):
+            resultMatrix[i][j] = matrix1[i][j] + matrix2[i][j]
+
+    return resultMatrix
+
+
+def multiply(matrix1, matrix2):
+    matrix1Rows, matrix1Cols = len(matrix1), len(matrix1[0])
+    matrix2Rows, matrix2Cols = len(matrix2), len(matrix2[0])
+
+    if matrix1Rows != matrix2Rows:
+        raise Exception("Matrix size mismatch")
+
+    resultMatrix = [[0 for _ in range(matrix2Cols)] for _ in range(matrix1Rows)]
+
+    for i in range(matrix1Rows):
+        for j in range(matrix2Cols):
+            for k in range(matrix1Cols):
+                resultMatrix[i][j] += matrix1[i][k] * matrix2[k][j]
+    return resultMatrix
+
+
+def subtract(matrix1, matrix2):
+    matrix1Rows, matrix1Cols = len(matrix1), len(matrix1[0])
+    matrix2Rows, matrix2Cols = len(matrix2), len(matrix2[0])
+
+    if matrix1Rows != matrix2Rows or matrix1Cols != matrix2Cols:
+        raise Exception("Matrix size mismatch")
+
+    resultMatrix = [[0 for _ in range(matrix1Cols)] for _ in range(matrix1Rows)]
+
+    for i in range(matrix1Rows):
+        for j in range(matrix1Cols):
+            resultMatrix[i][j] = matrix1[i][j] - matrix2[i][j]
+
+    return resultMatrix
+
+
+def determinant(matrix):
+    if len(matrix) != 3 or len(matrix[0]) != 3:
+        raise ValueError("Matrix must be 3x3")
+
+    a, b, c = matrix[0]
+    d, e, f = matrix[1]
+    g, h, i = matrix[2]
+
+    det = (a * (e * i - f * h)
+           - b * (d * i - f * g)
+           + c * (d * h - e * g))
+
+    return det
+
+
+def inverse(matrix):
+
+    det = determinant(matrix)
+    if det == 0:
+        raise ValueError("Matrix is singular and cannot be inverted")
+
+    a, b, c = matrix[0]
+    d, e, f = matrix[1]
+    g, h, i = matrix[2]
+
+    adjugate = [
+        [(e * i - f * h), -(b * i - c * h), (b * f - c * e)],
+        [-(d * i - f * g), (a * i - c * g), -(a * f - c * d)],
+        [(d * h - e * g), -(a * h - b * g), (a * e - b * d)]
+    ]
+
+    inverse_matrix = [[element / det for element in row] for row in adjugate]
+
+    return inverse_matrix
+
+
+def divide(matrix1, matrix2):
+    matrix2_inverse = inverse(matrix2)
+    return multiply(matrix1, matrix2_inverse)
+
+
+def read_matrix(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        matrix = [list(map(float, line.split())) for line in lines]
+    return matrix
+
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(' '.join(map(str, row)))
+
+    result = '\n'.join(' '.join(map(str, row)) for row in matrix)
+    with open('result.txt', 'w') as f:
+        f.write(result)
+
+
+print_matrix(divide(read_matrix('matrixA.txt'), read_matrix('matrixB.txt')))
+
+print('\n')
+
+print_matrix(multiply(read_matrix('matrixA.txt'), read_matrix('matrixB.txt')))
---
+++ matrixCalculator/vectorCalculator.py
@@ -0,0 +1,56 @@
+def add(vector1, vector2):
+    if len(vector1) != len(vector2):
+        raise Exception("Vector size mismatch")
+
+    result = []
+    for i in range(len(vector1)):
+        result.append(vector1[i] + vector2[i])
+    return result
+
+
+def subtract(vector1, vector2):
+    if len(vector1) != len(vector2):
+        raise Exception("Vector size mismatch")
+
+    result = []
+    for i in range(len(vector1)):
+        result.append(vector1[i] - vector2[i])
+    return result
+
+
+def dot_product(vector1, vector2):
+    if len(vector1) != len(vector2):
+        raise Exception("Vector size mismatch")
+
+    result = 0
+    for i in range(len(vector1)):
+        result += vector1[i] * vector2[i]
+    return result
+
+
+def divide(vector1, vector2):
+    if len(vector1) != len(vector2):
+        raise Exception("Vector size mismatch")
+
+    if any(b == 0 for b in vector2):
+        raise Exception("Division by zero encountered.")
+
+    result = []
+    for i in range(len(vector1)):
+        result.append(vector1[i] / vector2[i])
+    return result
+
+
+def read_vector(file_path):
+    with open(file_path, 'r') as f:
+        vector = f.read()
+        vector = [float(numbers) for numbers in vector.split(',')]
+    return vector
+
+
+def print_vector(result):
+    with open('vector_result', 'w') as file:
+        file.write(str(result))
+
+
+print_vector(dot_product(read_vector('vector1.txt'), read_vector('vector2.txt')))
