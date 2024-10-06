8597b71017b19420ea213a78862a43a27d8610f9
Vitalii Kovtun <VITALII.KOVTUN@lnu.edu.ua>
2024-10-06 23:04:45 +0300

    Error handling update.

--- abstraction/shape.py
+++ abstraction/shape.py
@@ -1,6 +1,10 @@
 from color import *
 
 NotImplemented_MSG = "Method Not Implemented"
+NoRadius_MSG = "'self' does not have a radius attribute."
+NoWidthHeight_MSG = "'self' does not have a width and height attribute."
+TypeError_MSG = "Type Error"
+ValueError_MSG = "Value Error"
 
 
 class Shape(ABC):
@@ -25,14 +29,35 @@ class Shape(ABC):
 
 class Circle(Shape):
     def __init__(self, radius: float, color: Color):
-        Shape.__init__(self, color)
-        self.radius = radius
+        try:
+            Shape.__init__(self, color)
+            self.radius = radius
+            if self.radius <= 0:
+                print(ValueError_MSG)
+                quit(1)
+        except TypeError:
+            print(TypeError_MSG)
+            quit(1)
 
     def area(self):
-        return 3.14 * self.radius ** 2
+        try:
+            return 3.14 * self.radius ** 2
+        except AttributeError:
+            print(NoRadius_MSG)
+            exit(1)
+        except TypeError:
+            print(TypeError_MSG)
+            exit(1)
 
     def perimeter(self):
-        return 2 * 3.14 * self.radius
+        try:
+            return 2 * 3.14 * self.radius
+        except AttributeError:
+            print(NoRadius_MSG)
+            exit(1)
+        except TypeError:
+            print(TypeError_MSG)
+            exit(1)
 
     def name(self):
         return "Circle"
@@ -40,15 +65,30 @@ class Circle(Shape):
 
 class Rectangle(Shape):
     def __init__(self, width: float, height: float, color: Color):
-        Shape.__init__(self, color)
-        self.width = width
-        self.height = height
+        try:
+            Shape.__init__(self, color)
+            self.width = width
+            self.height = height
+            if self.width <= 0 or self.height <= 0:
+                print(ValueError_MSG)
+                exit(1)
+        except TypeError:
+            print(TypeError_MSG)
+            exit(1)
 
     def area(self):
-        return self.width * self.height
+        try:
+            return self.width * self.height
+        except AttributeError:
+            print(NoWidthHeight_MSG)
+            exit(1)
 
     def perimeter(self):
-        return 2 * (self.width + self.height)
+        try:
+            return 2 * (self.width + self.height)
+        except AttributeError:
+            print(NoWidthHeight_MSG)
+            exit(1)
 
     def name(self):
         return "Rectangle"
@@ -56,7 +96,10 @@ class Rectangle(Shape):
 
 class Square(Rectangle):
     def __init__(self, side: float, color: Color):
-        Rectangle.__init__(self, side, side, color)
+        try:
+            Rectangle.__init__(self, side, side, color)
+        except TypeError:
+            print(TypeError_MSG)
 
     def name(self):
         return "Square"
